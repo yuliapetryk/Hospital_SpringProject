a569e10b45be06ecea362cb6b8fc45e6f46be550
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-05-24 15:01:33 +0300

    Create project

---
+++ src/main/java/hospital/HospitalApplication.java
@@ -0,0 +1,18 @@
+package com.example.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HospitalApplication.class, args);
+
+	}
+
+}
