2c2956634435c0ae315c18ac6430f2e5e9a1f4ec
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-06-09 14:28:02 +0300

    hide client secret

--- src/main/java/hospital/services/StaffService.java
+++ src/main/java/hospital/services/StaffService.java
@@ -20,11 +20,17 @@ public class StaffService {
     }
 
     public HttpResponse<String> getAllStaff() throws UnirestException {
-        return  Unirest.post("https://dev-p28zkraycjo5kb7x.eu.auth0.com/oauth/token")
-                .header("content-type", "application/json")
-                .body("{\"client_id\":\"qfEnDGWMic6hN9Dulvqzei9oX3wEuUko\",\"client_secret\":\"oc8HjJ7SSHnaY35HROw9Zp13_hUaGq0i7ZSgds_v3NpuKkszqC4rEjsR76SI2l1u\",\"audience\":\"https://dev-p28zkraycjo5kb7x.eu.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}")
-                .asString();
+        String clientId = System.getenv("AUTH0_CLIENT_ID");
+        String clientSecret = System.getenv("AUTH0_CLIENT_SECRET");
+        String audience = System.getenv("AUTH0_AUDIENCE");
+        
+        String requestBody = String.format("{\"client_id\":\"%s\",\"client_secret\":\"%s\",\"audience\":\"%s\",\"grant_type\":\"client_credentials\"}",
+                clientId, clientSecret, audience);
 
+        return Unirest.post("https://dev-p28zkraycjo5kb7x.eu.auth0.com/oauth/token")
+                .header("content-type", "application/json")
+                .body(requestBody)
+                .asString();
     }
 
     public Staff createStaff(Staff staff) {
